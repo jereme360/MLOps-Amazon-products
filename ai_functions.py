ddcf16d7586f59fac4fcbba45bce284d25102a48
jereme360 <jeremy.albright11@gmail.com>
2025-01-05 16:27:23 -0700

    more data cleaning on product data

--- ai_functions.py
+++ ai_functions.py
@@ -2,4 +2,5 @@ import pandas as pd
 import openai
 
 class LLMCommunication:
-    def get_analysis_plan(self, model_provider, )
+    def prompt_LLM(self, model_provider, prompt, system_message, image=None):
+        return None
