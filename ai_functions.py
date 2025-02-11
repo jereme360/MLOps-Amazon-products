955043a7153c9ddbf4c23efc4313d0a4d5ef38f5
jereme360 <jeremy.albright11@gmail.com>
2025-02-10 17:17:20 -0700

    finish extract features function

--- ai_functions.py
+++ ai_functions.py
@@ -1,11 +1,11 @@
-import transformers
 import torch
 from transformers import AutoModelForSequenceClassification, AutoTokenizer, AutoModel
 import numpy as np
 import pandas as pd
 import umap.umap_ as umap
+from sklearn.cluster import KMeans
 
-class ProductAttributeExtractor:
+class AttributeExtractor:
     def __init__(self, sentiment_model_name='distilbert-base-uncased', feature_model_name='BAAI/bge-small-en-v1.5'):
         self.feature_tokenizer = AutoTokenizer.from_pretrained(feature_model_name)
         self.feature_model = AutoModel.from_pretrained(feature_model_name)
@@ -13,7 +13,7 @@ class ProductAttributeExtractor:
         self.sentiment_tokenizer = AutoTokenizer.from_pretrained(sentiment_model_name)
         self.sentiment_model = AutoModelForSequenceClassification.from_pretrained(sentiment_model_name)
 
-    def extract_common_features(self, text_list, n_dimensions):
+    def extract_common_features(self, text_list, n_dimensions=50):
         '''
         Extracts common features from a list of product descriptions.
         Returns a list of n common features.
@@ -37,28 +37,31 @@ class ProductAttributeExtractor:
             embeddings = feature_outputs.last_hidden_state[:, 0].numpy()
 
         # Reduce dimensionality of embeddings
-        # JA LEFT OFF HERE
+        reducer = umap.UMAP(
+            n_components=n_dimensions,
+            n_neighbors=15,
+            min_dist=0.1,
+            random_state=42
+        )
+
+        reduced_embeddings = reducer.fit_transform(embeddings)
+
+        # Reduce to 2-D for visualization
+        reducer_2d = umap.UMAP(
+            n_components=2,
+            n_neighbors=15,
+            min_dist=0.1,
+            random_state=42
+        )
+
+        reduced_embeddings_2d = reducer_2d.fit_transform(embeddings)
 
         # Cluster embeddings to find common features
+        kmeans = KMeans(n_clusters=12, random_state=42)
 
-        # Create new cluster labels for each product description
+        # Return data frame with original text and cluster labels
+        text_df = pd.DataFrame(text_list, columns=['text'])
+        text_df['cluster'] = kmeans.fit_predict(reduced_embeddings)
 
-    def extract_product_features(self, product_description, common_features):
-        # (2) Determine each product's features from within the larger set
-        product_features = self.nlp(product_description)
-        relevant_features = self._filter_relevant_features(product_features, common_features)
-        return relevant_features
+        return text_df, reduced_embeddings_2d, reduced_embeddings
 
-    def categorize_sentiment(self, text):
-        # (3) Categorize sentiment on a 0-10 scale of negative to positive
-        sentiment = self.sentiment_analysis(text)[0]
-        score = self._convert_sentiment_to_scale(sentiment)
-        return score
-
-    def extract_common_reviewer_sentiments(self, reviews, n):
-        # (4) Determine a set of n common reviewer sentiments from all product reviews
-        sentiments = []
-        for review in reviews:
-            sentiments.append(self.sentiment_analysis(review))
-        common_sentiments = self._find_common_features(sentiments, n)
-        return common_sentiments
