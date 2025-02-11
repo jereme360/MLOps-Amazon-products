3b2df3cc2694765547972277e55ac3927db4a276
jereme360 <jeremy.albright11@gmail.com>
2025-02-10 18:11:39 -0700

    Add sentiment extracting pipeline

--- ai_functions.py
+++ ai_functions.py
@@ -13,13 +13,14 @@ class AttributeExtractor:
         self.sentiment_tokenizer = AutoTokenizer.from_pretrained(sentiment_model_name)
         self.sentiment_model = AutoModelForSequenceClassification.from_pretrained(sentiment_model_name)
 
-    def extract_common_features(self, text_list, n_dimensions=50):
+    def extract_common_features(self, text_series, n_dimensions=50):
         '''
         Extracts common features from a list of product descriptions.
         Returns a list of n common features.
         '''
         # Prepare data for feature extraction
-        text_clean = ["Represent this sentence for searching relevant passages: " + desc for desc in text_list]
+        text_clean = text_series.fillna("").str.strip()
+        text_clean = ["Represent this sentence for searching relevant passages: " + desc for desc in text_clean]
         
         # Tokenize texts
         encoded_inputs = self.feature_tokenizer(
@@ -33,7 +34,6 @@ class AttributeExtractor:
         # Create embeddings for each product description
         with torch.no_grad():
             feature_outputs = self.feature_model(**encoded_inputs)
-
             embeddings = feature_outputs.last_hidden_state[:, 0].numpy()
 
         # Reduce dimensionality of embeddings
@@ -59,9 +59,40 @@ class AttributeExtractor:
         # Cluster embeddings to find common features
         kmeans = KMeans(n_clusters=12, random_state=42)
 
-        # Return data frame with original text and cluster labels
-        text_df = pd.DataFrame(text_list, columns=['text'])
-        text_df['cluster'] = kmeans.fit_predict(reduced_embeddings)
+        # Prepare output
+        return pd.DataFrame({
+            'text': text_series,
+            'cluster': kmeans.fit_predict(reduced_embeddings),
+        }, index=text_series.index), reduced_embeddings_2d, reduced_embeddings
 
-        return text_df, reduced_embeddings_2d, reduced_embeddings
+    def extract_sentiment(self, text_series):
+        '''
+        Extracts sentiment from a list of product descriptions.
+        Returns a list of sentiment labels.
+        '''
+        # Preprocess text
+        text_clean = text_series.fillna("").str.strip()
+        
+        # Tokenize text
+        encoded = self.sentiment_tokenizer(
+            list(text_clean),
+            truncation=True,
+            padding=True,
+            max_length=512,
+            return_tensors='pt'
+        )
 
+        # Predict sentiment
+        with torch.no_grad():
+            output = self.sentiment_model(**encoded)
+
+        # Get predictions
+        predicted_label = torch.argmax(output.logits, dim=1).numpy()
+        predicted_prob = torch.softmax(output.logits, dim=1).numpy()
+
+        # Prepare output
+        return pd.DataFrame({
+            'text': text_series,
+            'sentiment': predicted_label,
+            'sentiment_prob': predicted_prob[:, 1]
+        }, index=text_series.index)
